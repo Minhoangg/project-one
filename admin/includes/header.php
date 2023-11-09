788dffe7c4cb80f7a0bdeb9dcdc4aba8c4473506
nhuhpc05472 <nhuhpc05472@fpt.edu.vn>
2023-11-09 14:06:46 +0700

    57-CatGiaoDien

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,20 +1,20 @@
 <div class="header">
             <div class="header-left">
-                <a href="index.html" class="logo"> <img src="../../content/contentAdmin/img/hotel_logo.png" width="50" height="70" alt="logo"> <span class="logoclass">HOTEL</span> </a>
-                <a href="index.html" class="logo logo-small"> <img src="../../content/contentAdmin/img/logo.png" alt="Logo" width="30" height="30"> </a>
+                <a href="" class="logo"> <img src="../../content/contentAdmin/img/hotel_logo.png" width="50" height="70" alt="logo"> <span class="logoclass">Quần Áo</span> </a>
+                <a href="" class="logo logo-small"> <img src="../../content/contentAdmin/img/logo.png" alt="Logo" width="30" height="30"> </a>
             </div>
             <a href="javascript:void(0);" id="toggle_btn"> <i class="fa-solid fa-bars"></i></a>
             <ul class="nav user-menu">
                 <li class="nav-item dropdown has-arrow">
-                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"> <span class="user-img"><img class="rounded-circle" src="assets/img/profiles/avatar-01.jpg" width="31" alt="Soeng Souy"></span> </a>
+                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"> <span class="user-img"><img class="rounded-circle" src="../../content/contentAdmin/img/profiles/avatar-01.jpg" width="31" alt="Soeng Souy"></span> </a>
                     <div class="dropdown-menu">
                         <div class="user-header">
-                            <div class="avatar avatar-sm"> <img src="assets/img/profiles/avatar-01.jpg" alt="User Image" class="avatar-img rounded-circle"> </div>
+                            <div class="avatar avatar-sm"> <img src="../../content/contentAdmin/img/profiles/avatar-01.jpg" alt="User Image" class="avatar-img rounded-circle"> </div>
                             <div class="user-text">
-                                <h6>Soeng Souy</h6>
-                                <p class="text-muted mb-0">Administrator</p>
+                                <h6>SHOP</h6>
+                                <p class="text-muted mb-0">Quản Lý</p>
                             </div>
-                        </div> <a class="dropdown-item" href="profile.html">My Profile</a> <a class="dropdown-item" href="settings.html">Account Settings</a> <a class="dropdown-item" href="login.html">Logout</a>
+                        </div> <a class="dropdown-item" href="index.php?pages=product&action=list">Danh Sách Sản Phẩm </a> <a class="dropdown-item" href="index.php?pages=account&action=forgot">Quên Mật Khẩu</a><a class="dropdown-item" href="index.php?pages=account&action=login">Đăng Nhập</a>
                     </div>
                 </li>
             </ul>
