174614311b5ad0aa31c271b4c7de114dd43ce520
nhuhpc05472 <nhuhpc05472@fpt.edu.vn>
2023-12-04 00:54:54 +0700

    QA_codegiaodienVN

--- view/page/about.php
+++ view/page/about.php
@@ -1,9 +1,9 @@
 	<!-- Title page -->
 	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('../content/contentCilent/images/bg-01.jpg');">
 		<h2 class="ltext-105 cl0 txt-center">
-			About
+			Giới Thiệu
 		</h2>
-	</section>	
+	</section>
 
 
 	<!-- Content page -->
@@ -13,19 +13,20 @@
 				<div class="col-md-7 col-lg-8">
 					<div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
 						<h3 class="mtext-111 cl2 p-b-16">
-							Our Story
+							Chuyện của chúng tôi
 						</h3>
 
 						<p class="stext-113 cl6 p-b-26">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat consequat enim, non auctor massa ultrices non. Morbi sed odio massa. Quisque at vehicula tellus, sed tincidunt augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas varius egestas diam, eu sodales metus scelerisque congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas gravida justo eu arcu egestas convallis. Nullam eu erat bibendum, tempus ipsum eget, dictum enim. Donec non neque ut enim dapibus tincidunt vitae nec augue. Suspendisse potenti. Proin ut est diam. Donec condimentum euismod tortor, eget facilisis diam faucibus et. Morbi a tempor elit.
+							Nếu bạn đang muốn trau dồi tri thức, thay đổi phong cách sống thì hãy thử nói chuyện với bộ óc tuyệt vời và lòng dũng cảm của Lucy Siegle qua cuốn sách To Die For: Is Fashion Wearing Out The World nhé! Cuốn sách này như một cánh cửa mới đưa bạn tới đại dương bao la, rộng lớn để hiểu hơn về những gì mà các công nhân làm việc trong các nhà máy dệt may phải chịu đựng tại một số nơi như: Bangladesh, Campuchia, Uzbekistan,…
 						</p>
 
 						<p class="stext-113 cl6 p-b-26">
-							Donec gravida lorem elit, quis condimentum ex semper sit amet. Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut fringilla turpis in vehicula vehicula. Pellentesque congue ac orci ut gravida. Aliquam erat volutpat. Donec iaculis lectus a arcu facilisis, eu sodales lectus sagittis. Etiam pellentesque, magna vel dictum rutrum, neque justo eleifend elit, vel tincidunt erat arcu ut sem. Sed rutrum, turpis ut commodo efficitur, quam velit convallis ipsum, et maximus enim ligula ac ligula. 
+							To Die For: Is Fashion Wearing Out The World sẽ vén tấm màn đen tối, để lộ bức tranh thực tế khốc liệt mà ngành thời trang nhanh gây ra không phải ai cũng biết. Faslink nghĩ rằng, bạn nhất định sẽ thay đổi nhận thức, suy nghĩ về vấn đề đạo đức tiêu dùng của bản thân sau khi đọc qua cuốn sách này.
 						</p>
 
 						<p class="stext-113 cl6 p-b-26">
-							Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
+
+							Có câu hỏi nào không? Hãy cho chúng tôi biết tại cửa hàng tại tầng 8, 379 Hudson St, New York, NY 10018 hoặc gọi cho chúng tôi theo số (+1) 96 716 6879
 						</p>
 					</div>
 				</div>
@@ -38,25 +39,26 @@
 					</div>
 				</div>
 			</div>
-			
+
 			<div class="row">
 				<div class="order-md-2 col-md-7 col-lg-8 p-b-30">
 					<div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
 						<h3 class="mtext-111 cl2 p-b-16">
-							Our Mission
+							
+Nhiệm vụ của chúng ta
 						</h3>
 
 						<p class="stext-113 cl6 p-b-26">
-							Mauris non lacinia magna. Sed nec lobortis dolor. Vestibulum rhoncus dignissim risus, sed consectetur erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam maximus mauris sit amet odio convallis, in pharetra magna gravida. Praesent sed nunc fermentum mi molestie tempor. Morbi vitae viverra odio. Pellentesque ac velit egestas, luctus arcu non, laoreet mauris. Sed in ipsum tempor, consequat odio in, porttitor ante. Ut mauris ligula, volutpat in sodales in, porta non odio. Pellentesque tempor urna vitae mi vestibulum, nec venenatis nulla lobortis. Proin at gravida ante. Mauris auctor purus at lacus maximus euismod. Pellentesque vulputate massa ut nisl hendrerit, eget elementum libero iaculis.
+						Cho dù khách hàng không mua sản phẩm đó nhưng nhiệm vụ của người nhân viên tư vấn bán hàng thời trang vẫn là giới thiệu các sản phẩm mới đến với khách hàng. Không chỉ vậy, giới thiệu về những sản phẩm có trong cửa hàng cũng là điều quan trọng cần phải ghi nhớ.
 						</p>
 
 						<div class="bor16 p-l-29 p-b-9 m-t-22">
 							<p class="stext-114 cl6 p-r-40 p-b-11">
-								Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while.
+							Với một công ty hay cửa hàng thời trang thì doanh số là điều gần như quan trọng nhất để duy trì hoạt động. Vậy nguồn doanh thu này đến từ đâu? Chắc chắn nguồn thu sẽ đến nhiều nhất từ việc bán hàng trực tiếp. Vì vậy, làm sao để thuyết phục khách hàng, đặc biệt là khách hàng khó tính mua đồ là điều những nhân viên tư vấn bán hàng sẽ phải làm.
 							</p>
 
 							<span class="stext-111 cl8">
-								- Steve Job’s 
+							https://blog.topcv.vn/
 							</span>
 						</div>
 					</div>
@@ -71,4 +73,4 @@
 				</div>
 			</div>
 		</div>
-	</section>	
+	</section>
