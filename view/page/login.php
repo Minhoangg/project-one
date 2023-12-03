8a6da82337737155da25805da7af56c786afc8d8
huienmyy <mynvhpc07527@fpt.edu.vn>
2023-12-04 00:22:52 +0700

    QA_09_Đăng Nhập

--- view/page/login.php
+++ view/page/login.php
@@ -55,15 +55,49 @@ if (isset($_GET["code"])) {
     }
 }
 
+
+
+if (isset($_POST["btn_login"])) {
+    $user = new user();
+    $phone_number = $_POST["username"];
+    $password = $_POST["pass"];
+    $infor_phone = $user->selectByphone($phone_number);
+
+    if (!isset($infor_phone) && !empty($phone_number)) {
+        $error_username = 'Số điện thoại không tồn tại';
+    }
+    if (empty($phone_number)) {
+        $loiten = 'Số điện thoại không được để trống';
+    }
+    if (empty($password)) {
+        $loimatkhau = 'Mật khẩu không được bỏ trống';
+    }
+    if (!preg_match('/^\d{10}$/', $phone_number) && !empty($phone_number)) {
+        $loisdt = "Số điện thoại không hợp lệ (phải có đúng 10 số)";
+    }
+    if (!isset($loiten) && !isset($loisdt) && !isset($loimatkhau) && !isset($error_username)) {
+        $checkuser = $user->checkUser($phone_number, $password);
+        if ($checkuser) {
+            $_SESSION['user'] = $checkuser;
+            header("Location: index.php?pages=home");
+        } else {
+            $error_not_account = "Tên đăng nhập hoặc mật khẩu không đúng";
+        }
+    }
+}
+
 ?>
+
+
+
 
 <div class="limiter">
     <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
         <div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
-            <form class="login100-form validate-form flex-sb flex-w">
-					<span class="login100-form-title p-b-53">
-						Đăng nhập
-					</span>
+            <form method="post" class="login100-form validate-form flex-sb flex-w">
+                <span class="login100-form-title p-b-53">
+                    Đăng nhập
+                </span>
 
                 <a href="#" class="btn-face m-b-20">
                     <i class="fa fa-facebook-official"></i>
@@ -75,45 +109,65 @@ if (isset($_GET["code"])) {
                 </a>
 
                 <div class="p-t-31 p-b-9">
-						<span class="txt1">
-							Tên đăng nhập
-						</span>
+                    <span class="txt1">
+                        Số điện thoại
+                    </span>
                 </div>
                 <div class="wrap-input100 validate-input" data-validate="Username is required">
                     <input class="input100" type="text" name="username">
                     <span class="focus-input100"></span>
-                </div>
 
+                    </input>
+                </div>
+                <div class="w-100 p-t-13 p-b-9">
+                    <p style="color:red">
+                        <?php
+                        if (isset($loisdt)) {
+                            echo $loisdt;
+                        }
+                        if (isset($error_username)) {
+                            echo $error_username;
+                        }
+                        if (isset($loiten)) {
+                            echo $loiten;
+                        }
+                        ?>
+                    </p>
+                </div>
                 <div class="p-t-13 p-b-9">
-						<span class="txt1">
-							Mật khẩu
-						</span>
+                    <span class="txt1">
+                        Mật khẩu
+                    </span>
 
                 </div>
                 <div class="wrap-input100 validate-input" data-validate="Password is required">
                     <input class="input100" type="password" name="pass">
                     <span class="focus-input100"></span>
+
                 </div>
+                <p style="color:red">
+                    <?php if (isset($loimatkhau))
+                        echo $loimatkhau ?>
+                    </p>
+                    <div class="container-login100-form-btn m-t-17">
+                        <button class="login100-form-btn" name="btn_login">
+                            Đăng nhập
+                        </button>
+                    </div>
 
-                <div class="container-login100-form-btn m-t-17">
-                    <button class="login100-form-btn">
-                        Đăng nhập
-                    </button>
-                </div>
-
-                <a href="#" class="txt2 bo1 m-l-5">
-                    Quên mật khẩu?
-                </a>
-                <div class="w-full text-center p-t-55">
-						<span class="txt2">
-							Bạn chưa có tài khoảng?
-						</span>
-
-                    <a href="#" class="txt2 bo1">
-                        Đăng ký
+                    <a href="#" class="txt2 bo1 m-l-5">
+                        Quên mật khẩu?
                     </a>
-                </div>
-            </form>
+                    <div class="w-full text-center p-t-55">
+                        <span class="txt2">
+                            Bạn chưa có tài khoản?
+                        </span>
+
+                        <a href="#" class="txt2 bo1">
+                            Đăng ký
+                        </a>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
